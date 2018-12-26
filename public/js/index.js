0a16675b421528140dfb8e07ad064018fc8de0aa
Matthew Marshall Huntsberry <Matthew.Marshall.Huntsberry@ibm.com@matthews-mbp.austin.ibm.com>
2018-12-26 13:55:36 -0600

    Added styling to header/nav bar.  Added in hamburger with animation @ mobile and tablet view breakpoints.

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+	let checkbox = document.querySelector('.nav-toggle');
+	let checkboxLabel = document.querySelector('.nav-toggle-label');
+
+	function removeActive() {
+		checkboxLabel.classList.remove('nav-toggle-label--active');
+	}
+
+	checkbox.onclick = function() {
+		if (checkbox.checked) {
+			checkboxLabel.classList.add('nav-toggle-label--active');
+		} else {
+			removeActive();
+		}
+	};
+
+	let menu = document.querySelector('.menu');
+	menu.onclick = function() {
+		removeActive();
+		checkbox.checked = false;
+	};
+});
